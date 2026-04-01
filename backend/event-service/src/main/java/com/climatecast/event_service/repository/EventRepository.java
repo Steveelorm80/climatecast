ffc5c3145b5a8144d3855ba0c30713c57fc1492f
Stephen Kwaku Pometsey <steveelorm@gmail.com>
2026-04-01 23:16:54 +0100

    remove mongodb

--- backend/event-service/src/main/java/com/climatecast/event_service/repository/EventRepository.java
+++ backend/event-service/src/main/java/com/climatecast/event_service/repository/EventRepository.java
@@ -1,8 +1,8 @@
-package com.climatecast.event_service.repository;
+// package com.climatecast.event_service.repository;
 
-import com.climatecast.event_service.model.Event;
-import org.springframework.data.mongodb.repository.MongoRepository;
+// import com.climatecast.event_service.model.Event;
+// import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EventRepository extends MongoRepository<Event, String> {
-}
+// public interface EventRepository extends MongoRepository<Event, String> {
+// }
 
